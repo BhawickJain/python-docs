95fb4a2167eb8193c0b9404e430c1899b3185e69
BhawickJain <bhawick@outlook.com>
2021-10-24 12:05:26 +0100

    moved modules

---
+++ src/fib.py
@@ -0,0 +1,35 @@
+# Fibonacci numbers module
+
+__all__ = ['fib']
+
+def fib(n):
+    """
+    write Fibanacci series up to n
+    """
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+    
+def fib2(n):
+    """
+    return Fibonacci serires up to n
+    """
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+
+# give custom name other than its filename
+# __name__ = 'fibo'
+
+# run only when run directly
+if __name__ == '__main__':
+    import sys
+    fib(int(sys.argv[1]))
+
+
