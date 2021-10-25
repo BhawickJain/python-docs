6eb02a4656f091e7fd00fcccaaff823bdbdad325
BhawickJain <bhawick@outlook.com>
2021-10-25 19:05:23 +0100

    migrated example source code to pyExamples module

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-# Constants
-
-PI = 3.145
-C = 299792358 # ms^-1 Speed of light
-K = 1.380649e-23 # JK^-1 Boltzmann Constant
-G = 6.6743015e-11 # m^3 kg^-1 s
-RINF = 10973731.56816021 # m^-1 Rydberg constant
-
-   
